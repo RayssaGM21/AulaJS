05f881c30e92b67a571e0d40b41930293b15d303
Rayssa Marconato <rayssa.g.marconato@gmail.com>
2025-05-22 21:10:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log("Oi mundo!")
+function chamarOiMundo(){
+    console.log("Oi mundo!")    
+}
+
+const modal = document.getElementById("modal")
+const formulario = document.getElementById("study-form")
+
+// inputs
+const materiaInput = document.getElementById("materia")
+const dataInput = document.getElementById("data")
+const corInput = document.getElementById("cor")
+
+let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+console.log(tarefas)
+
+
+function abrirModal() {
+    modal.classList.add('open');
+}
+
+function fecharModal() {
+    modal.classList.remove('open');
+}
+
+function salvar() {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
+
+// arrow function
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const novaTarefa = {
+        materia: materiaInput.value,
+        data: dataInput.value,
+        cor: corInput.value
+    }
+
+    tarefas.push(novaTarefa);
+    salvar()
+    console.log(tarefas)
+
+
+})
+
